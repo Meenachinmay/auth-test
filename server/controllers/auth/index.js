802fc4b19bf2d6ab2f5cd27c5a3a9a673e822eaa
アナンダ沈明 <meenachinmay@anandachinakiranoMacBook-Air.local>
2022-01-03 04:19:23 +0530

    register user data with validation and duplicate emails are being saved now in database

--- server/controllers/auth/index.js
+++ server/controllers/auth/index.js
@@ -1,5 +1,29 @@
-exports.register = (req, res) => {
-    res.json({
-        data: "Hello",
-    })
+const  User = require('../../models/user.model')
+
+exports.register = async (req, res) => {
+    const { username, email, password } = req.body
+
+    const user = await User.findOne({email: email});
+    if ( user ) {
+        return res.status(400).json({
+            error: "Email is already taken"
+        })
+    }
+    
+    let newUser = await new User({username, email, password})
+
+    if (newUser) {
+        await newUser.save()
+        
+        return res.status(200).json({
+            message: "register success",
+            newuser: newUser
+        })
+    } else {
+        return res.status(400).json({
+            message: "register failed",
+            error: "Server error"
+        })
+    }
+    
 }
