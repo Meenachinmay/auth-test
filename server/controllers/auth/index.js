1e0eed2b33db50b93bcecc84d27c82c91c301168
アナンダ沈明 <meenachinmay@anandachinakiranoMacBook-Air.local>
2022-01-03 14:09:30 +0530

    account activation email sent

--- server/controllers/auth/index.js
+++ server/controllers/auth/index.js
@@ -1,4 +1,8 @@
 const  User = require('../../models/user.model')
+const jwt = require ('jsonwebtoken')
+const sendgrid = require ('@sendgrid/mail')
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
 
 exports.register = async (req, res) => {
     const { username, email, password } = req.body
@@ -14,7 +18,7 @@ exports.register = async (req, res) => {
 
     if (newUser) {
         await newUser.save()
-        
+
         return res.status(200).json({
             message: "register success",
             newuser: newUser
@@ -26,4 +30,41 @@ exports.register = async (req, res) => {
         })
     }
     
+}
+
+// send email to user to activate then save the data to the database
+exports.regsiterUsingEmailActivation = async (req, res) => {
+    const { username, email, password } = req.body
+
+    const user = await User.findOne({email: email});
+    if ( user ) {
+        return res.status(400).json({
+            error: "Email is already taken"
+        })
+    }
+
+    const token = jwt.sign({username, email, password}, process.env.JWT_ACCOUNT_ACTIVATION, {expiresIn: '10m'})
+
+    const emailData = {
+        from: process.env.EMAIL_FROM,
+        to: email,
+        subject: `Account activation link`,
+        html: `
+            <p>Use following link to activate your account!</p>
+            <p> ${process.env.CLIENT_URL}/auth/activate${token}</p>
+            <hr />
+        `
+    }
+
+    sendgrid.send(emailData)
+        .then(sent => {
+            return res.json({
+                message: 'Activation link has been sent successfully!'
+            })
+        })
+        .catch(err => {
+            return res.json({
+                error: err
+            })
+        })
 }
--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -3,12 +3,12 @@ const express = require ('express')
 const router = express.Router()
 
 //
-const { register } = require ('../../controllers/auth/index')
+const { regsiterUsingEmailActivation } = require ('../../controllers/auth/index')
 
 //
 const { runvalidator } = require('../../validators/index')
 
-router.post('/create-new-user', runvalidator, register)
+router.post('/create-new-user', runvalidator, regsiterUsingEmailActivation)
 
 
 module.exports = router
