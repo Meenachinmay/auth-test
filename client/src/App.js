ee02990898e510688f0de6d1af4f81e7e08fd8fb
Chinmay Anand <chinmayanand896@gmail.com>
2022-01-04 20:57:45 +0530

    react router added and tested working with navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import Navbar from './core/Navbar';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Profile from './core/Profile';
+import Login from './core/Login';
+
 const App = () => {
   return (
     <div className="">
+      <>
       <Navbar />
-      <div> hello </div>
+      </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/profile" element={ <Profile /> } />
+          <Route path="/login" element={ <Login /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
