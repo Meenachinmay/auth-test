d4bbd74a1f7aa92004e40dc60296e7e83461ad36
Chinmay Anand <chinmayanand896@gmail.com>
2022-01-04 17:29:11 +0530

    tainwindcss navbar added for auth testing at client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import Navbar from './core/Navbar';
 
 const App = () => {
   return (
-    <div className="bg-blue-500">
-      <div className="flex items-center justify-between container mx-auto text-3xl text-gray-900">
-        <h1>Hello world</h1>
-      </div>
+    <div className="">
+      <Navbar />
+      <div> hello </div>
     </div>
   )
 }
---
+++ client/src/core/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+
+    return (
+    <div className="bg-blue-500 flex items-center justify-between p-7 text-white">
+        <div className="cursor-pointer">Real connect</div>
+        <div className="flex items-center">
+            <div className="mx-2 cursor-pointer">login</div>
+            <div className="mx-2 cursor-pointer">Register</div>
+        </div>
+    </div>
+    )
+}
+
+export default Navbar;
