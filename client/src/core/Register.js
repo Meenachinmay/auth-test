097a60db9b9f2e3e49d79f538d24da4a8680c0c5
Chinmay Anand <chinmayanand896@gmail.com>
2022-01-04 23:05:12 +0530

    register request on sign up API are successfully tested from react client to nodejs backend

--- client/src/core/Register.js
+++ client/src/core/Register.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import axios from 'axios'
 
 const Register = () => {
+    
+    const [values, setValues] = useState({
+        username:'',
+        email:'',
+        password:'',
+        buttonText:''
+    })
+
+    const { username, email, password, buttonText } = values
+
+    const handleChange = (name) => (event) =>{
+        setValues({...values, [name]: event.target.value})
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+
+        axios({
+            method: 'POST',
+            url: `http://localhost:8000/apiV1/create-new-user`,
+            data: { username, email, password }
+        })
+        .then(response => {
+            console.log("activate account via email", response.data)
+        })
+        .catch(error => {
+            console.log(error.response.data)
+        })
+    }
+
     return (
-        <div>Hello world from Register route</div>
+        <form>
+            <div>
+                <lable>Name</lable>
+                <input onChange={handleChange('username')} value={username} type="text" placeholder='name'/>
+                <lable>Email</lable>
+                <input onChange={handleChange('email')} value={email} type="text" placeholder='email'/>
+                <lable>Password</lable>
+                <input onChange={handleChange('password')} value={password} type="password" placeholder='password'/>
+
+                <button className='bg-red-500 font-bold text-white border-1 p-5 rounded mx-5' onClick={handleSubmit}>Submit</button>
+            </div>
+        </form>
     )
 }
 
